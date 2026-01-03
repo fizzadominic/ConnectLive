eac6f259f0269b7ffff440351503f90d396fae17
Fizza Dominic <83395329+fizzadominic@users.noreply.github.com>
2026-01-03 19:28:09 +0500

    initial commit

---
+++ BACKEND/src/server.js
@@ -0,0 +1,26 @@
+
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.auth.routes.js";
+
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+
+app.use("/api/messages", messageRoutes);
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`);
+});
+
+
+
