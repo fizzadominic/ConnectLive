a7d62401d637340c05669ec353afb019d05c6c00
Fizza Dominic <83395329+fizzadominic@users.noreply.github.com>
2026-01-10 17:44:15 +0500

    rate-limiting implemented

--- BACKEND/src/routes/auth.routes.js
+++ BACKEND/src/routes/auth.routes.js
@@ -1,16 +1,24 @@
 import express from 'express';
-import { login, signup } from '../controllers/auth.controller.js';
+import { login, signup, logout , updateProfile } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { arcjetProtection } from '../middleware/arcjet.middleware.js';
 
 const router = express.Router();
 
+
+router.use(arcjetProtection);
 
 router.post("/signup", signup);
 
 router.get("/login", login);
 
-router.get("/logout", (req, res)=>{
-    res.send("logout endpoint")
-});
+router.get("/logout",logout);
+
+// if user wants to update a profile they must be authenticated 
+router.put("/profile-update", protectRoute , updateProfile);
+
+router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
+
 
 
 export default router;
